a56e6687852510dffd18dd4105bcd4df3036b912
Benji Mathew <benmat92@gmail.com>
2022-01-27 05:22:38 -0600

    Working. Login/registration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Deals from './components/Deals';
+import DealLoadingComponent from './components/DealLoading';
+import axiosInstance from './axios';
 
+/*
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const DealLoading = DealLoadingComponent(Deals);
+	const [appState, setAppState] = useState([]);
+	const [error, setError] = useState();
+
+
+	useEffect(() => {
+		setAppState({ loading: true });
+		const apiUrl = `http://127.0.0.1:8000`;
+		fetch(apiUrl)
+			.then((data) => data.json())
+			.then((deals) => {
+				setAppState({ loading: false, deals: deals })
+			.catch((err) => {
+	console.log(err);
+});
+			});
+	}, []);
+	console.log(appState.deals);
+	if (error || !Array.isArray(appState.deals)) {
+    return <p>There was an error loading your data!</p>;
+  }
+	return (
+		<div className="App">
+			<h1>Latest Deals</h1>
+			<DealLoading isLoading={appState.loading} deals={appState.deals} />
+		</div>
+	);
 }
 
+export default App;
+*/
+
+function App() {
+	const DealLoading = DealLoadingComponent(Deals);
+	const [data, setData] = useState([]);
+	const [appState, setAppState] = useState({
+		loading: true,
+		deals: 'null',
+});
+
+	const [error, setError] = useState();
+
+	useEffect(() => {
+		axiosInstance.get().then((res) => {
+			const allDeals = res.data;
+			setAppState({ loading: false, deals: allDeals });
+			console.log(res.data);
+
+		});
+	}, [setAppState]);
+	if (error || !Array.isArray(appState.deals)) {
+		return <p>There was an error loading your data!</p>;
+	}
+	return (
+		<div className="App">
+			<h1>Latest Deals</h1>
+			<DealLoading isLoading={appState.loading} deals={appState.deals} />
+		</div>
+	);
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import Header from './components/header';
+import Footer from './components/footer';
+import Register from './components/register';
+import Login from './components/login';
+import Logout from './components/logout';
+import Single from './components/single';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const routing = (
+	<Router>
+		<React.StrictMode>
+  		<Header />
+    	<Routes>
+    		  <Route exact path="/" element={<App />} />
+  				<Route path="/register/" element={<Register />} />
+  				<Route path="/login/" element={<Login />} />
+  				<Route path="/logout/" element={<Logout />} />
+					<Route path="/deal/:slug" element={<Single />} />
+    	</Routes>
+  		<Footer />
+		</React.StrictMode>
+	</Router>
 );
 
+ReactDOM.render(routing, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
